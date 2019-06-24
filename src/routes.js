863799566b2671647e93270e672a60454890cc00
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2019-06-25 01:00:31 +0200

    Dashboard matches

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from './HoC/Layout';
-import {Switch, Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+
+import PrivateRoute from './components/auth_routes/privateRoutes';
+import PublicRoute from './components/auth_routes/publicRoutes';
 
 import Home from './components/home';
 import SignIn from './components/signin';
 
 import Dashboard from './components/admin/Dashboard';
-
+import AdminMatches from './components/admin/matches/index';
 
 const Routes = (props) => {
 
@@ -15,9 +18,10 @@ const Routes = (props) => {
         <div>
             <Layout>
                 <Switch>
-                    <Route exact component={Home} path='/'/>
-                    <Route exact component={SignIn} path='/sign_in'/>
-                    <Route exact component={Dashboard} path='/dashboard'/>
+                    <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>
+                    <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/>
+                    <PublicRoute {...props} restricted={false} path='/' exact component={Home}/>
+                    <PublicRoute {...props} restricted={true} path='/sign_in' exact component={SignIn}/>
                 </Switch>
             </Layout>
         </div>
